6adc968ba03d05fec89bf00140819578c220042b
onsentawake <58753727+onsentawake@users.noreply.github.com>
2020-03-15 23:27:25 +0900

    Update nepalikitai.py

--- nepalikitai.py
+++ nepalikitai.py
@@ -1,17 +1,22 @@
-#インポおと
+#import
 import random
 
+
 #グロ変
-nepal =["ビザOK","入国OK","ビザNG","入国拒否"]
-mission =["ビザ取得ミッション","フライトチェンジミッション","パスポートミッション","入国ミッション"]
+nepal =["ビザOK","俺は神","ビザNG","入国拒否"]
 
 
 def plan():
+
   print("Namaste number dinuss")
   print("コロナウイルスによって人生ハードモードになりましたひひひ")
   print()
 
+
+
 def travel():
+
+  mission =["ビザ取得ミッション","フライトチェンジミッション","パスポートミッション","入国ミッション"]
   for bbb in mission:
     print(bbb)
   print()
@@ -19,14 +24,33 @@ def travel():
 
 
 def flight_cancel():
-  input('祈りながらプレスエニーキー')
-  result = random.randint(0,3)
-  if result == 0 or result == 1:
-    print(nepal[0])
-  else:
-    print(f'あなたは{nepal[2]}のため、{nepal[3]}になりましたひひひひ')
-  
 
-plan()
-travel()
-flight_cancel()
+  input("祈りながらプレスエニーキー")
+  print()
+  result =  random.randint(0,3)
+  
+  if result == 1:
+    print(nepal[0])
+    print()
+
+  elif result == 2:
+    print("バンコクで足止め")
+    print()
+
+  else:
+    print(f'あなたは{nepal[1]}だと思っていたのに、{nepal[2]}のため、{nepal[3]}になりましたひひひひ')
+    print()
+
+  
+#本文
+
+aaa = 0
+
+while aaa < 5:
+  unko = random.randint(0,2)
+  plan()
+  travel()
+  flight_cancel()
+  if unko == 1:
+    break
+  aaa += 1
